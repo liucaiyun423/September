24149df822fbcbff7d04dbcb5024bdb11d338db5
caiyun liu <liucaiyun423@me.com>
2017-09-27 14:56:09 -0400

    add credit on server side

--- server/models/mongo/user.js
+++ server/models/mongo/user.js
@@ -15,5 +15,10 @@ module.exports = (sequelize, DataTypes) => {
 */
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-const userSchema = new Schema({googleId : String});
+
+const userSchema = 
+  new Schema({googleId : String,
+              credits : {type: Number, default: 0}
+            });
+
 mongoose.model('users', userSchema);
--- server/routes/billing.js
+++ server/routes/billing.js
@@ -4,13 +4,17 @@ const stripe = require('stripe')(keys.stripSecretKey);
 module.exports = (app) => {
     app.post('/api/stripe', 
        async (req,res)=>{
+           if(!req.user){
+               return res.status(401).send({error: 'You must be logged in!'});
+           }
            const charge = await stripe.charges.create({
                amount: 500,
                currency: 'usd',
                description: '5 dollars for 5 credits',
                source: req.body.token.id
            });  
-           console.log("billing token: " ,req.body.token);
-           console.log("charge object: ", charge);
+           req.user.credits += 5;
+           const user = await req.user.save();
+           res.send(user);
         });
 }
