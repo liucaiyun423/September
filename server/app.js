875ae6dec0f783ca15bf5991309c8c87726430fb
caiyun liu <liucaiyun423@me.com>
2017-09-28 10:22:23 -0400

    refactor login requirement to middleware

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
--- server/routes/billing.js
+++ server/routes/billing.js
@@ -1,12 +1,11 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
     app.post('/api/stripe', 
-       async (req,res)=>{
-           if(!req.user){
-               return res.status(401).send({error: 'You must be logged in!'});
-           }
+        requireLogin,
+        async (req,res)=>{
            const charge = await stripe.charges.create({
                amount: 500,
                currency: 'usd',
